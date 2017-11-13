bd05a72982ea0fad04742e4c5bb0b96264557a0d
Max Zelinka <fourthwalljoke@gmail.com>
2017-11-13 19:13:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,24 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 const index = 0
 
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which)
-  if(key === code[index]) {
-    index++;
-
-  if(index === code.length) {
-    alert("You did it!")
-    index = 0
-  }
-} else {
-  index = 0
-}
-
-}
 
 
 function init() {
  const input = document.querySelector("body")
- input.addEventListner('keydown', onKeyDownHandler)
+ input.addEventListner('keydown', function onKeyDownHandler(e) {
+   const key = parseInt(e.detail || e.which)
+   if(key === code[index]) {
+     index++;
+
+   if(index === code.length) {
+     alert("You did it!")
+     index = 0
+   }
+ } else {
+   index = 0
+ }
+
+ }
+
 
 }
